9a8fc7744a34545c58cf5ee2a07c48b38406f00f
Noah Amoo <yawnoah3n@yahoo.com>
2025-08-25 16:18:09 +0100

    Added TMDB API

---
+++ services/api.ts
@@ -0,0 +1,39 @@
+export const TMDB_CONFIG = {
+    BASE_URL: 'https://api.themoviedb.org/3',
+    API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
+    Headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`
+    }
+}
+
+export const fetchPopularMovies = async({ querry }: { querry: string}) => {
+    const endpoint = '/discover/movie?sort_by=popularity.desc';
+
+    const response = await fetch(endpoint, {
+        method: 'GET',
+        headers: TMDB_CONFIG.Headers
+    })
+
+    if (!response.ok) {
+        throw new Error(`Failed to fetch movies: ${response.statusText}`)
+    }
+
+    const data = await response.json();
+
+    return data.results;
+}
+
+// const url = 'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1';
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     accept: 'application/json',
+//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiODg2ZjNkZWVhMDVjOWM1MzhkNTdhMzMwYjEzNTYyMyIsIm5iZiI6MTc1NjEzMDIyMi42ODMsInN1YiI6IjY4YWM2YmFlM2E0ZTQxOTAyYjFlNTYwYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ekvJ0Nj_PZmyc3E4tsqPertVpdixd0lJ32RdP0Ubs8g'
+//   }
+// };
+
+// fetch(url, options)
+//   .then(res => res.json())
+//   .then(json => console.log(json))
+//   .catch(err => console.error(err));
