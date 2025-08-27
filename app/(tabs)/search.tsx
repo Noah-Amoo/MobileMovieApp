73b858b264f5ae06b69f75f4f810ff20b43fd559
Noah Amoo <yawnoah3n@yahoo.com>
2025-08-27 08:19:50 +0100

    Initialized text search in the text input onn the search screen

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -4,18 +4,22 @@ import { icons } from '@/constants/icons'
 import { images } from '@/constants/images'
 import { fetchMovies } from '@/services/api'
 import useFetch from '@/services/useFetch'
-import { useRouter } from 'expo-router'
-import React from 'react'
-import { ActivityIndicator, FlatList, Image, View, Text } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { ActivityIndicator, FlatList, Image, Text, View } from 'react-native'
 
 const Search = () => {
-  const router = useRouter();
+
+  const [searchQuerry, setSearchQuerry] = useState('')
 
   const { 
     data: movies, 
     loading: moviesLoading, 
-    error: moviesError 
-  } = useFetch(() => fetchMovies({ query: ''}))
+    error: moviesError,
+  } = useFetch(() => fetchMovies({ query: searchQuerry}), false)
+
+  useEffect(() => {
+    
+  }, [searchQuerry])
 
   return (
     <View className='flex-1 bg-primary'>
@@ -39,7 +43,11 @@ const Search = () => {
                 <Image source={icons.logo} className='w-12 h-10' />
             </View>
             <View className='my-5'>
-              <SearchBar placeholder='Search movies...' />
+              <SearchBar 
+                placeholder='Search movies...' 
+                value={searchQuerry}
+                onChangeText={(text: string) => setSearchQuerry(text)}
+              />
             </View>
 
             {moviesLoading && (
@@ -52,12 +60,18 @@ const Search = () => {
               </Text>
             )}
 
-            {!moviesLoading && !moviesError && 'SEARCH TERM'.trim() && movies?.length > 0 && (
+            {/* Always show search phrase when query is not empty */}
+            {searchQuerry.trim() && (
               <Text className='text-xl text-white font-bold'>
-                Search Results for {' '}
-                <Text className='text-accent'>SEARCH TERM</Text>
+                Search Results for <Text className='text-accent'>{searchQuerry}</Text>
               </Text>
             )}
+
+            {/* Show "no results" message if query is not empty and no movies returned */}
+            {!moviesLoading && !moviesError && searchQuerry.trim() && movies?.length === 0 && (
+              <Text className='text-light-300 mt-2'>No results found.</Text>
+            )}
+
           </>
         }
       />
